3be7a563352e07b23698f64e3b0c1e5ded9d0ddb
Mikael Henriksson <mikael@zoolutions.se>
2013-10-15 15:46:17 +0200

    Only work with the selection if it returns a hash

--- lib/wash_out/soap_actions.rb
+++ lib/wash_out/soap_actions.rb
@@ -1,15 +1,20 @@
 module WashOut
   class SoapActions
     extend Forwardable
-    def_delegators :@soap_actions, :[], :[]=, :sort, :each, :keys
+    def_delegators :@soap_actions, :[], :[]=, :sort, :each, :keys, :invert, :select
 
     def initialize(options = {})
       @soap_actions = options
     end
 
     def fetch(action)
-      action_spec   = @soap_actions[action]
-      action_spec ||= @soap_actions.invert.select{|k,v| k[:raw_action] == action }.first.first
+      action_spec = self[action]
+
+      unless action_spec
+        action_spec = self.invert.select{|k,v| k[:raw_action] == action }
+        action_spec = action_spec.keys[0] if action_spec.is_a?(Hash)
+      end
+
       action_spec
     end
   end
