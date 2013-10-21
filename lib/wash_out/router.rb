8fefb488117c3136f8d6f712be8482599e538f42
Boris Staal <boris@staal.io>
2013-10-21 20:26:47 +0700

    Better routing when header is missing (fixes #120)

--- lib/wash_out/router.rb
+++ lib/wash_out/router.rb
@@ -15,10 +15,10 @@ module WashOut
     def parse_soap_action(env)
       return env['wash_out.soap_action'] if env['wash_out.soap_action']
 
-      soap_action = env['HTTP_SOAPACTION']
+      soap_action = env['HTTP_SOAPACTION'].to_s.gsub(/^"(.*)"$/, '\1')
 
       if soap_action.blank?
-        soap_action = parse_soap_parameters(env)
+        soap_action = nori.parse(soap_body env)
             .values_at(:envelope, :Envelope).compact.first
             .values_at(:body, :Body).compact.first
             .keys.first.to_s
@@ -29,25 +29,33 @@ module WashOut
 
       if controller.soap_config.namespace
         namespace = Regexp.escape controller.soap_config.namespace.to_s
-        soap_action.gsub!(/^"?(#{namespace}(\/|#)?)?([^"]*)"?$/, '\3')
-      else
-        soap_action = soap_action[1...-1] if soap_action.starts_with?('"')
+        soap_action.gsub!(/^(#{namespace}(\/|#)?)?([^"]*)$/, '\3')
       end
 
       env['wash_out.soap_action'] = soap_action
     end
 
-    def parse_soap_parameters(env)
-      return env['wash_out.soap_data'] if env['wash_out.soap_data']
-
-      nori_parser = Nori.new(
+    def nori(snakecase=false)
+      Nori.new(
         :parser => controller.soap_config.parser,
         :strip_namespaces => true,
         :advanced_typecasting => true,
-        :convert_tags_to => ( controller.soap_config.snakecase_input ? lambda { |tag| tag.snakecase.to_sym } : lambda { |tag| tag.to_sym } ))
+        :convert_tags_to => (
+          snakecase ? lambda { |tag| tag.snakecase.to_sym } 
+                    : lambda { |tag| tag.to_sym }
+        )
+      )
+    end
 
-      env['wash_out.soap_data'] = if env['rack.input'].respond_to? :string then env['rack.input'].string else env['rack.input'].read end
-      env['wash_out.soap_data'] = nori_parser.parse(env['wash_out.soap_data'])
+    def soap_body(env)
+      env['rack.input'].respond_to?(:string) ? env['rack.input'].string
+                                             : env['rack.input'].read
+    end
+
+    def parse_soap_parameters(env)
+      return env['wash_out.soap_data'] if env['wash_out.soap_data']
+
+      env['wash_out.soap_data'] = nori(controller.soap_config.snakecase_input).parse(soap_body env)
       references = WashOut::Dispatcher.deep_select(env['wash_out.soap_data']){|k,v| v.is_a?(Hash) && v.has_key?(:@id)}
 
       unless references.blank?
