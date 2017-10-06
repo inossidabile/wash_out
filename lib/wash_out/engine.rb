4f2a783727476f932aaff288b4aa87caf00f3f0a
rada bogdan <raoul_ice@yahoo.com>
2017-10-06 17:16:37 +0300

    fix adding middleware for rails 5.1.0 (#234)

--- lib/wash_out/engine.rb
+++ lib/wash_out/engine.rb
@@ -1,10 +1,9 @@
-
 module WashOut
   class Engine < ::Rails::Engine
     config.wash_out = ActiveSupport::OrderedOptions.new
     initializer "wash_out.configuration" do |app|
       if app.config.wash_out[:catch_xml_errors]
-        app.config.middleware.insert_after 'ActionDispatch::ShowExceptions', WashOut::Middleware
+        app.config.middleware.insert_after(ActionDispatch::ShowExceptions, WashOut::Middleware)
       end
     end
 
